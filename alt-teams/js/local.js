4f7cab9dc76f5616e13b958aabfc3d2c7e628afa
Rahul Bhardwaj <36123351+rajputrahul445@users.noreply.github.com>
2024-02-26 23:23:27 +0530

    Add files via upload

---
+++ alt-teams/js/local.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){
+      $('.slider').owlCarousel({
+          loop:false,
+          margin:20,
+          nav:true,
+          autoplay:true,
+          autoplayTimeout:10000,
+          dots:true,
+          navText : ["<img src='images/arrow-left.png'/>","<img src='images/arrow.png'/>"],
+          responsive:{
+              0:{
+                  items:1
+              },
+              600:{
+                  items:1
+              },
+              1000:{
+                  items:1
+              }
+          }
+      });
+    $('.stepSlider').owlCarousel({
+          loop:false,
+          margin:0,
+          nav:true,
+          autoplay:false,
+          autoplayTimeout:5000,
+          dots:false,
+          navText : ["<img src='images/step-prev.png'/>","<img src='images/step-next.png'/>"],
+          responsive:{
+              0:{
+                  items:1
+              },
+              600:{
+                  items:1
+              },
+              1000:{
+                  items:1
+              }
+          }
+      });
+   
+    $('.threeSlider').owlCarousel({
+          loop:true,
+          margin:0,
+          nav:false,
+          center: true,
+          autoplay:true,
+          autoplayTimeout:5000,
+          dots:true,
+          navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+          responsive:{
+              0:{
+                  items:1,
+                  center:false
+              },
+              600:{
+                  items:2,
+                  center:false
+              },
+              1000:{
+                  items:3
+              }
+          }
+      });
+    });
+
+
+
+$( ".navbar-toggler, .cross-toggler" ).click(function() {
+    $(".collapse").toggleClass("active");
+    $(".overLay").toggleClass("active");
+  });
+
+$( ".closeSticky" ).click(function() {
+    $(".stickyMenu").hide();
+  });
+if(window.innerWidth < 991){
+  $( ".dropmenuToggle" ).click(function() {
+    $(this).parent(".dropdown").toggleClass("active");
+  });
+}
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    if (scroll >= 41) {
+        $(".mainHeader").addClass("fixedHeader");
+    }else{
+    	$(".mainHeader").removeClass("fixedHeader");
+    }
+});
